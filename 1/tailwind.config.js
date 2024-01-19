1d58636639613525c921118e1180cfb96319036c
Fakkrp203 <138076555+Fakkrp203@users.noreply.github.com>
2024-01-19 14:13:50 -0300

    FrontEnd Mentor Exercises

---
+++ 1/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        White: " hsl(0, 0%, 100%)",
+        LightGray: "hsl(212, 45%, 89%)",
+        GrayishBlue: "hsl(220, 15%, 55%)",
+        DarkBlue: "hsl(218, 44%, 22%)",
+      },
+    },
+  },
+  plugins: [],
+};
